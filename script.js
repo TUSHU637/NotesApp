8c777409eb89c87043b9a9668e7bc2d9b5ac555c
Tushar Gupta <101970891+TUSHU637@users.noreply.github.com>
2024-05-06 23:50:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+let addBtn=document.querySelector(".addButton");
+let container=document.querySelector(".container");
+
+addBtn.addEventListener("click",(e)=>{
+    createNoteBox();
+    saveNotes();
+    
+})
+function saveNotes(){
+let allNotes=document.querySelectorAll(".noteBox textarea");
+let data=[];
+allNotes.forEach((notes)=>{
+    data.push(notes.value);
+})
+if(data.length===0){
+    localStorage.removeItem("note");
+}
+else{
+localStorage.setItem("note",JSON.stringify(data));
+}
+}
+function createNoteBox(text=""){
+    
+    let newNoteBox=document.createElement("div");
+    newNoteBox.setAttribute("class","noteBox");
+    newNoteBox.innerHTML= `
+    <div class="tools">
+        <div class="save">
+            <span class="save material-icons">
+                save
+             </span>
+        </div>
+        <div class="delete"><span class="del material-icons">
+            delete
+            </span></div>
+    </div>
+    <textarea spellcheck="false" placeholder="write your notes...." class="text-area">${text}</textarea>
+   
+
+`;
+
+
+container.appendChild(newNoteBox);
+
+newNoteBox.querySelector(".del").addEventListener("click",(e)=>{
+    newNoteBox.remove();
+    saveNotes();
+})
+newNoteBox.querySelector(".save").addEventListener("click",(e)=>{
+    saveNotes();
+})
+newNoteBox.querySelector("textarea").addEventListener("focusout",(e)=>{
+    saveNotes();
+})
+}
+
+//page refreshing function
+(
+function(){
+   let lsNote=JSON.parse(localStorage.getItem("note"));
+   if(lsNote===null){
+    createNoteBox();
+   }
+   else{
+   lsNote.forEach((item)=>{
+    createNoteBox(item);
+    saveNotes();
+   })
+   
+   }
+}()
+)
